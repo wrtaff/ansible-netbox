d650bacf08977383cd04d3c86ef1db00806b1cbb
will <wrtaff@gmail.com>
2026-09-07 15:23:11 -0400

    polybar: use custom/script module for date with click action to copy timestamp (#4514)

--- files/polybar/copy_timestamp.sh
+++ files/polybar/copy_timestamp.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+export DISPLAY=${DISPLAY:-:0}
 TS=$(date --iso-8601=seconds)
-printf "%s" "$TS" | xclip -selection primary -in
-printf "%s" "$TS" | xclip -selection clipboard -in
-notify-send "Copied Timestamp" "$TS" -t 2000 2>/dev/null || true
+echo -n "$TS" | xclip -selection primary
+echo -n "$TS" | xclip -selection clipboard
+echo -n "$TS" | xsel --primary --input 2>/dev/null || true
+echo -n "$TS" | xsel --clipboard --input 2>/dev/null || true
+notify-send -u normal -t 2000 "Timestamp Copied" "$TS" 2>/dev/null || true
