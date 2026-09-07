65c3e2aeb8a796780e71c0dbfa004b3c97ed5bd7
will <wrtaff@gmail.com>
2026-09-07 14:55:37 -0400

    feat(polybar): add install_polybar playbook and multi-monitor config (Trac #4514)

---
+++ files/polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
+
+# Launch Polybar on all connected monitors
+if type "xrandr" >/dev/null 2>&1; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main 2>&1 | logger -t polybar &
+  done
+else
+  polybar --reload main 2>&1 | logger -t polybar &
+fi
